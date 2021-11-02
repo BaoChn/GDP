9aac7e0899aec312ca11e68de9145a26eb942fcf
宝昌 <32792217+BaoChn@users.noreply.github.com>
2021-11-02 21:14:05 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'f91b8f04409a4026a9950b9a78083500',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Graduation Design Project(individual)',
+  domain: 'https://gdp.one',
+  author: 'BaoChn',
 
   // open graph metadata (optional)
-  description: 'Example site description',
+  description: '《观梦》个人毕设时间线',
   socialImageTitle: 'Transitive Bullshit',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'BaoChn',
+  github: 'BaoChn',
+  linkedin: 'BaoChn',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
